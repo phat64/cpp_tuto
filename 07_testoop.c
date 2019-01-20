610a82413fb39a89f4f9744a8f312e039250355a
phat64 <msx64@yahoo.fr>
2019-01-20 14:05:13 +0100

    07_testoop.c : 2nd commit -  in progress - need to be cleaned

--- 07_testoop.c
+++ 07_testoop.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 
 
+enum ACTOR_TYPE
+{
+	HERO = 0,
+	COIN,
+	ENEMI,
+	BOSS,
+	MAX_ACTOR_TYPE
+};
+
 typedef struct actor
 {
 	int type;
@@ -24,7 +33,7 @@ actor_t* hero_new()
 
 	if (a)
 	{
-		a->type = 0;
+		a->type = HERO;
 		a->update = hero_update;
 		a->render = hero_render;
 		a->destroy = hero_destroy;
@@ -49,16 +58,131 @@ void hero_destroy()
 	free((void*)ACTOR_THIS);
 }
 
+void coin_update(float deltaTime);
+void coin_render();
+void coin_destroy();
+
+actor_t* coin_new()
+{
+	actor_t * a = (actor_t *)calloc(1, sizeof(actor_t));
+
+	if (a)
+	{
+		a->type = COIN;
+		a->update = coin_update;
+		a->render = coin_render;
+		a->destroy = coin_destroy;
+	}
+
+	return a;
+}
+
+void coin_update(float deltaTime)
+{
+	printf("coin_update\n");
+}
+
+void coin_render()
+{
+	printf("coin_render\n");
+}
+
+void coin_destroy()
+{
+	printf("coin_destroy\n");
+	free((void*)ACTOR_THIS);
+}
+
+void enemi_update(float deltaTime);
+void enemi_render();
+void enemi_destroy();
+
+actor_t* enemi_new()
+{
+	actor_t * a = (actor_t *)calloc(1, sizeof(actor_t));
+
+	if (a)
+	{
+		a->type = ENEMI;
+		a->update = enemi_update;
+		a->render = enemi_render;
+		a->destroy = enemi_destroy;
+	}
+
+	return a;
+}
+
+void enemi_update(float deltaTime)
+{
+	printf("enemi_update\n");
+}
+
+void enemi_render()
+{
+	printf("enemi_render\n");
+}
+
+void enemi_destroy()
+{
+	printf("enemi_destroy\n");
+	free((void*)ACTOR_THIS);
+}
+
+void boss_update(float deltaTime);
+void boss_render();
+void boss_destroy();
+
+actor_t* boss_new()
+{
+	actor_t * a = (actor_t *)calloc(1, sizeof(actor_t));
+
+	if (a)
+	{
+		a->type = BOSS;
+		a->update = boss_update;
+		a->render = boss_render;
+		a->destroy = boss_destroy;
+	}
+
+	return a;
+}
+
+void boss_update(float deltaTime)
+{
+	printf("boss_update\n");
+}
+
+void boss_render()
+{
+	printf("boss_render\n");
+}
+
+void boss_destroy()
+{
+	printf("boss_destroy\n");
+	free((void*)ACTOR_THIS);
+}
 
 // actor factory
 actor_t * actor_create(int type)
 {
-	return hero_new();
+	typedef actor_t* (*actor_new_callback)();
+
+	static actor_new_callback actor_new_array[MAX_ACTOR_TYPE] =
+	{
+		hero_new,
+		coin_new,
+		enemi_new,
+		boss_new
+	};
+
+	return actor_new_array[type]();
 }
 
 int main()
 {
 	actor_t * hero = actor_create(0);
+	actor_t * list[8];
 
 	INSTANCE_ACTOR(hero)->update(1.0f/60.0f);
 	INSTANCE_ACTOR(hero)->render();
