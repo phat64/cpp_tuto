1c21c715c9d9be3a2bdb850a85ec49257665e6b6
phat64 <msx64@yahoo.fr>
2019-08-11 21:16:37 +0200

    09_testinterpreter.cpp : add valid combo in CheckCombo : {OPERATOR, FUNCTION_NAME} and {FUNCTION_END, OPERATOR}

--- 09_testinterpreter.cpp
+++ 09_testinterpreter.cpp
@@ -331,7 +331,7 @@ bool CheckCombo(const vector<Token> & tokens, int idx0, int idx1)
 	const char * validcombo[] = {"((", "))",
 		"(N", "N)", "NO", "ON", "O(", ")O",
 		"F[", ")]", "[(", "[]", ",(", "),",
-		"[N", "N]", "N,", ",N", NULL};
+		"[N", "N]", "N,", ",N", "OF", "]O", NULL};
 
 	for (int i = 0; validcombo[i]; i++)
 	{
