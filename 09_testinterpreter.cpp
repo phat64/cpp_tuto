d3c96a91187ab718a9ebdf07e0c936a73355a99f
phat64 <msx64@yahoo.fr>
2019-08-16 23:10:58 +0200

    09_testinterpreter.cpp : add 'I(' in validcombo in CheckCombo(...) - means 'if (' is valid

--- 09_testinterpreter.cpp
+++ 09_testinterpreter.cpp
@@ -345,7 +345,8 @@ bool CheckCombo(const vector<Token> & tokens, int idx0, int idx1)
 	const char * validcombo[] = {"((", "))",
 		"(N", "N)", "NO", "ON", "O(", ")O",
 		"F[", "[F", ",F", "],", ")]", "]], ""[(", "[]", ",(", "),",
-		"[N", "N]", "N,", ",N", "OF", "]O", NULL};
+		"[N", "N]", "N,", ",N", "OF", "]O",
+		"I(", NULL};
 
 	for (int i = 0; validcombo[i]; i++)
 	{
