28e5b103e3b0db5f5eee41f10555a08eaa1588df
phat64 <msx64@yahoo.fr>
2019-11-02 00:04:48 +0100

    09_testinterpreter.cpp : add 'UN' and 'UUN' in Compute2(...) and Compute3(...)

--- 09_testinterpreter.cpp
+++ 09_testinterpreter.cpp
@@ -1116,6 +1116,18 @@ bool Compute2(double & result, double val, const vector<Token> & tokens, int fir
 			}*/
 			return true;
 		}
+
+		// [TODO] remove it : temp code cuz UNARY_OPERATOR are replaced by (binary) OPERATOR
+		if (token0.type == UNARY_OPERATOR)
+		{
+			double a = token1.GetDoubleValue();
+			switch(token0.strvalue[0])
+			{
+				case '-': result = -a; return true; break;
+				case '!': result = !a; return true;break;
+				default : printf("error unary operator"); result = 0.0;
+			}
+		}
 	}
 	return false;
 }
@@ -1148,6 +1160,16 @@ bool Compute3(double & result, const vector<Token> & tokens, int first)
 		if (token1.type == NUMBER || token1.type == VARIABLE_NAME || token1.type == STRING)
 		{
 			result = token1.GetDoubleValue();
+			return true;
+		}
+
+		// [TODO] remove it : temp code cuz UNARY_OPERATOR are replaced by (binary) OPERATOR
+		// !!N
+		if ((token0.type == UNARY_OPERATOR && token0.strvalue[0] == '!')
+			&& (token1.type == UNARY_OPERATOR  && token1.strvalue[0] == '!')
+			&& token2.cvalue == 'N')
+		{
+			result = !!token2.GetDoubleValue();
 			return true;
 		}
 	}
